581c9e018f3cc35e5f92b7d9bbda549147629168
Sangwon Hyun <robohyun66@gmail.com>
2019-07-24 23:22:35 -0700

    Experimentally trying out rslurm, replacing mclapply in parallel cv.

--- flowcy/R/parallel-cv.R
+++ flowcy/R/parallel-cv.R
@@ -68,7 +68,20 @@ parallel_cv.covarem <- function(ylist = ylist, X = X,
     cat("clump", iclump, "consists of rows:", ialphas, fill=TRUE)
 
     ## Traverse from right->left, from the right edge
-    new.reslists = mclapply(ialphas, function(ialpha){
+    ## new.reslists = mclapply(ialphas, function(ialpha){
+    ##   warmstart = loadres(ialpha, gridsize, destin)
+    ##   ## cat("Warmstart from (", ialpha, gridsize, ")", fill = TRUE)
+    ##   move_to_left(ialpha, (gridsize-1):1,
+    ##                pie_lambdas, mean_lambdas,
+    ##                gridsize,
+    ##                warmstart, destin, ylist, X, mysplits,
+    ##                nsplit, refit,
+    ##                multicore.cv = multicore.cv,
+    ##                ...)
+    ## }, mc.cores = numfork)
+
+    ## Start of experimental
+    onealpha <- function(ialpha){
       warmstart = loadres(ialpha, gridsize, destin)
       ## cat("Warmstart from (", ialpha, gridsize, ")", fill = TRUE)
       move_to_left(ialpha, (gridsize-1):1,
@@ -78,7 +91,11 @@ parallel_cv.covarem <- function(ylist = ylist, X = X,
                    nsplit, refit,
                    multicore.cv = multicore.cv,
                    ...)
-    }, mc.cores = numfork)
+    }
+    new.reslists = slurm_apply(ialphas, onealpha,
+                               nodes = 2, cpus_per_node = floor(numfork / 2) )
+    ## End of experimental.
+
     cat(fill=TRUE)
   }
 }
